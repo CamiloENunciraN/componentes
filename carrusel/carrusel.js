dcc25b60d08eb376d4af72be9ed92c2002765efd
CamiloENunciraN <camiloeduardonn@ufps.edu.co>
2023-03-27 20:45:04 -0500

    carrusel

---
+++ carrusel/carrusel.js
@@ -0,0 +1,7 @@
+
+
+function abrirImagen(id){
+let imagenMostrar=document.getElementById('0');
+let imagenVer=document.getElementById(id);
+imagenMostrar.src=imagenVer.src;
+}
---
+++ login/login.js
@@ -0,0 +1,17 @@
+
+
+//al dar en ingresar
+ function ingresar() {
+
+let correo=document.getElementById('correo').value;
+let contra=document.getElementById('contrasena').value;
+
+//validacion de los datos
+if(correo==""){
+  alert("ingrese correo");
+}else if(contra==""){
+  alert("ingrese contraseña");
+}else{
+
+}
+}
